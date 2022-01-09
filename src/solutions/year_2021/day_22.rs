690c09e4a2f0cc7e61a5e6d582bba8a55d11647e
Eric Lifka <eric.lifka@gmail.com>
2022-01-09 11:19:33 -0500

    2021:22

--- src/solutions/year_2021/day_22.rs
+++ src/solutions/year_2021/day_22.rs
@@ -1,122 +1,127 @@
 use crate::input;
+use std::{cmp, collections::VecDeque, ops::{Not, BitAnd, Sub}};
 
-#[derive(Debug)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 struct Cuboid {
-    x_min: i32, x_max: i32,
-    y_min: i32, y_max: i32,
-    z_min: i32, z_max: i32,
+    x_min: i64, x_max: i64,
+    y_min: i64, y_max: i64,
+    z_min: i64, z_max: i64,
 }
 
-enum Overlap {
-    None,
-    Contains,
-    Contained,
-    Overlap(Cuboid),
+enum Slice { X(i64), Y(i64), Z(i64) }
+enum Toggle { On, Off }
+struct Instruction(Cuboid, Toggle);
+
+impl Not for Cuboid {
+    type Output = bool;
+
+    // True if this cuboid is invalid, False if it's valid
+    fn not(self) -> Self::Output {
+        self.x_max < self.x_min ||
+        self.y_max < self.y_min ||
+        self.z_max < self.z_min
+    }
+}
+
+impl BitAnd for Cuboid {
+    type Output = Self;
+
+    fn bitand(self, other: Self) -> Self::Output {
+        Self {
+            x_min: cmp::max(self.x_min, other.x_min),
+            x_max: cmp::min(self.x_max, other.x_max),
+            y_min: cmp::max(self.y_min, other.y_min),
+            y_max: cmp::min(self.y_max, other.y_max),
+            z_min: cmp::max(self.z_min, other.z_min),
+            z_max: cmp::min(self.z_max, other.z_max),
+        }
+    }
+}
+
+impl Sub for Cuboid {
+    type Output = VecDeque<Self>;
+
+    fn sub(self, other: Self) -> Self::Output {
+        let left = self.slice_down(Slice::X(other.x_min - 1));
+        let right = self.slice_up(Slice::X(other.x_max + 1));
+
+        let middle = 
+            self.slice_up(Slice::X(other.x_min))
+                .slice_down(Slice::X(other.x_max));
+
+        let top_middle = middle.slice_up(Slice::Y(other.y_max + 1));
+        let bottom_middle = middle.slice_down(Slice::Y(other.y_min - 1));
+
+        let center =
+            middle.slice_up(Slice::Y(other.y_min))
+                  .slice_down(Slice::Y(other.y_max));
+
+        let center_front = center.slice_down(Slice::Z(other.z_min - 1));
+        let center_back = center.slice_up(Slice::Z(other.z_max + 1));
+
+        let mut slices = VecDeque::from([
+            left, right, top_middle, bottom_middle, center_front, center_back
+        ]);
+        
+        slices.retain(|&cuboid| !!cuboid);
+
+        slices
+    }
 }
 
 impl Cuboid {
-    fn compare(&self, other: &Cuboid) -> Overlap {
-        Overlap::None
-    }
-
-    fn fragment(&self, cutout: &Cuboid) -> Vec<Cuboid> {
-        vec![]
-    }
-}
-
-enum Toggle { On, Off }
-
-#[derive(Debug)]
-struct Instruction(Cuboid, Toggle);
-
-#[derive(Debug)]
-enum List {
-    Nil,
-    Node {
-        item: Cuboid,
-        next: Box<List>,
-    },
-}
-
-struct ListIterator {
-    head: &List,
-    cur: Option<&List>,
-}
-
-impl List {
-    fn next(&self) -> &List {
-        switch self {
-            Nil => Nil,
-            Node { next, .. } => next,
+    fn slice_down(&self, slice: Slice) -> Self {
+        match slice {
+            Slice::X(at) => Self { x_max: at, ..*self },
+            Slice::Y(at) => Self { y_max: at, ..*self },
+            Slice::Z(at) => Self { z_max: at, ..*self },
         }
     }
-}
 
-impl ListIterator {
-    fn new(head: &List) -> ListIterator {
-        ListIterator { head, cur: None }
-    }
-
-    fn remove(&self) {
-        match self.cur {
-            None => None,
-            Some(List::Nil) => None,
-            Some(List::Node { item, next }) => {
-                
-            }
+    fn slice_up(&self, slice: Slice) -> Self {
+        match slice {
+            Slice::X(at) => Self { x_min: at, ..*self },
+            Slice::Y(at) => Self { y_min: at, ..*self },
+            Slice::Z(at) => Self { z_min: at, ..*self },
         }
     }
-}
 
-impl Iterator for ListIterator {
-    type Item = &Cuboid;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.cur = match self.cur {
-            None => Some(head),
-            Some(cur) => Some(cur.next()),
-        };
-
-        match self.cur {
-            None => None,
-            Some(List::Nil) => None,
-            Some(List::Node { item, .. }) => item,
-        }
+    fn area(&self) -> i64 {
+        (self.x_max - self.x_min) * (self.y_max - self.y_min) * (self.z_max - self.z_min)
     }
 }
 
 pub fn run() {
-    let mut existing_cuboids = List::Nil;
+    let mut existing_cuboids: VecDeque<Cuboid> = VecDeque::new();
 
     for Instruction(cuboid, toggle) in parse_input() {
-        let iter = ListIterator::new(existing_cuboids);
-        for shape in iter {
-            match cuboid.compare(&shape) {
-                Overlap::None => None,
-                Overlap::Contains => iter.remove(),
-                Overlap::Contained => break,
-                Overlap::Overlap(overlap) => {
-                    iter.remove();
-                    for new_cuboid in shape.fragment(overlap) {
-                        existing_cuboids = List::Node {
-                            item: new_cuboid,
-                            next: Box::new(existing_cuboids),
-                        };
-                    }
-                },
+        let mut new_cuboids: VecDeque<Cuboid> = VecDeque::new();
+
+        existing_cuboids.retain(|&shape| {
+            let overlap = cuboid & shape;
+            if !overlap {
+                true
+            } else {
+                if overlap != shape {
+                    new_cuboids.append(&mut (shape - overlap));
+                }
+                false
             }
-        }
+        });
         
         match toggle {
-            Toggle::Off => None,
+            Toggle::Off => (),
             Toggle::On => {
-                existing_cuboids = List::Node {
-                    item: cuboid,
-                    next: Box::new(existing_cuboids),
-                };
+                new_cuboids.push_back(cuboid);
             },
         };
+
+        existing_cuboids.append(&mut new_cuboids);
     }
+
+    let area = existing_cuboids.iter().fold(0, |acc, c| acc + c.area());
+
+    println!(" Part 2: {}", area);
 }
 
 // input format:
@@ -156,7 +161,7 @@ fn parse_cuboid(input: &str) -> Cuboid {
 
 // input format:
 //   x=-27831..-17345
-fn parse_range(input: &str) -> (i32, i32) {
+fn parse_range(input: &str) -> (i64, i64) {
     let parts = input[2..].split("..").collect::<Vec<&str>>();
     let min = parts[0];
     let max = parts[1];
