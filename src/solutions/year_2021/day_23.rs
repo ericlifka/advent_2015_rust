8e9eacd68a16b637974453b711782ba3f429acd6
Eric Lifka <eric.lifka@gmail.com>
2022-01-23 23:05:11 -0500

    running but either not finishing or scaling too slowly

--- src/solutions/year_2021/day_23.rs
+++ src/solutions/year_2021/day_23.rs
@@ -1,33 +1,26 @@
 use std::fmt::{Display,Formatter,Result};
+use std::collections::VecDeque;
+
 /*
-sample:
-#############
-#...........#
-###B#C#B#D###
-  #A#D#C#A#
-  #########
+Plays puzzle game with board like this:
+╔═══════════╗
+║e:        0║
+╠═══════════╣
+║           ║
+╚═╣B║A║A║D╠═╝
+  ║D║C║B║A║
+  ║D║B║A║C║
+  ║B║C║D║C║
+  ╚═╩═╩═╩═╝
 
-input:
-#############
-#...........#
-###B#A#A#D###
-  #D#C#B#A#
-  #D#B#A#C#
-  #B#C#D#C#
-  #########
-
-  BBDA
-  DCBA
-  DBAC
-  CADC
-
-#############
-#...........#
-###B#B#D#A###
-  #D#C#B#A#
-  #D#B#A#C#
-  #C#A#D#C#
-  #########
+array index reference:
+╔═════════════════════════════════╗
+║ 0  1  2  3  4  5  6  7  8  9 10 ║
+╚════╗ 11 ╔╗ 15 ╔╗ 19 ╔╗ 23 ╔═════╝
+     ║ 12 ║║ 16 ║║ 20 ║║ 24 ║
+     ║ 13 ║║ 17 ║║ 21 ║║ 25 ║
+     ║ 14 ║║ 18 ║║ 22 ║║ 26 ║
+     ╚════╩╩════╩╩════╩╩════╝
 */
 
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -35,6 +28,7 @@ enum Pod {
     A, B, C, D,
     Empty,
 }
+use Pod::*;
 
 impl Display for Pod {
     fn fmt(&self, f: &mut Formatter) -> Result {
@@ -108,24 +102,24 @@ impl Board {
         let mut to = end;
 
         if let Empty = moving_pod { // can't move if there's no pod there
-            println!("target starting space was empty");
+            // println!("target starting space was empty");
             return None;
         }
         if from == to {
-            println!("starting and ending space were the same");
+            // println!("starting and ending space were the same");
             return None;
         }
         if from < 11 && to < 11 { // a piece can't move from the hallway to another spot in the hallway
-            println!("start and end both in hallway");
+            // println!("start and end both in hallway");
             return None;
         }
         if self.space[to].occupied() { // quick abort if the target space isn't empty
-            println!("target space wasn't empty");
+            // println!("target space wasn't empty");
             return None;
         }
         match to {
             2 | 4 | 6 | 8 => { // can't stop outside a home
-                println!("tried to stop outside a room");
+                // println!("tried to stop outside a room");
                 return None;
             },
             _ => {}
@@ -140,7 +134,7 @@ impl Board {
             for offset in 0..index % 4 {
             // while offset >= 0 {
                 if self.space[ col * 4 + offset + 11 ].occupied() {
-                    println!("ran into pod on the way out of room");
+                    // println!("ran into pod on the way out of room");
                     return None;
                 }
 
@@ -165,7 +159,7 @@ impl Board {
             };
 
             if moving_pod != target_type { // can only move into their own home
-                println!("pod trying to go into the wrong room");
+                // println!("pod trying to go into the wrong room");
                 return None;
             }
 
@@ -174,12 +168,12 @@ impl Board {
                     spaces_moved += 1;
 
                     if self.space[ col * 4 + offset + 11 ].occupied() {
-                        println!("ran into occupied space in target room");
+                        // println!("ran into occupied space in target room");
                         return None;
                     }
                 } else {                // spaces after the target space need to be only pod types that belong here
                     if self.space[ col * 4 + offset + 11 ] != target_type {
-                        println!("room has pods of the wrong type in it, can't move in.");
+                        // println!("room has pods of the wrong type in it, can't move in.");
                         return None;
                     }
                 }
@@ -189,7 +183,7 @@ impl Board {
         }
 
         if from == to { // can't move between different spots within the same home
-            println!("start and end both within the same room");
+            // println!("start and end both within the same room");
             return None;
         }
 
@@ -204,7 +198,7 @@ impl Board {
             spaces_moved += 1;
 
             if self.space[from].occupied() {
-                println!("Ran into occupied space in hallway");
+                // println!("Ran into occupied space in hallway");
                 return None;
             }
         }
@@ -217,20 +211,55 @@ impl Board {
         board.space[end] = moving_pod;
         board.space[start] = Empty;
 
-        println!("Moved {} from {} to {} and spent {} energy", moving_pod, start, end, energy);
+        // println!("Moved {} from {} to {} and spent {} energy", moving_pod, start, end, energy);
 
         Some(board)
     }
 
-    fn get_all_possible_moves(&self) -> Vec<Board> {
-        let mut moves = Vec::new();
+    fn pod_can_move(&self, index: usize) -> bool {
+        // quick check to verify a pod can move before spending the time to find all of its destinations
+        self.space[index].occupied() && 
+        match index {
+            0  => self.space[1].empty(),
+            1  => self.space[11].empty() || self.space[3].empty(),
+            2  => false, // can't be here
+            3  => self.space[11].empty() || self.space[15].empty() || self.space[5].empty(),
+            4  => false, // can't be here
+            5  => self.space[3].empty() || self.space[15].empty() || self.space[19].empty() || self.space[7].empty(),
+            6  => false, // can't be here
+            7  => self.space[5].empty() || self.space[19].empty() || self.space[23].empty(),
+            8  => false, // can't be here
+            9  => self.space[7].empty() || self.space[23].empty(),
+            10 => self.space[10].empty(),
+            11 => self.space[1].empty() || self.space[3].empty(),
+            12 => self.space[11].empty(),
+            13 => self.space[12].empty(),
+            14 => self.space[13].empty(),
+            15 => self.space[3].empty() || self.space[5].empty(),
+            16 => self.space[15].empty(),
+            17 => self.space[16].empty(),
+            18 => self.space[17].empty(),
+            19 => self.space[5].empty() || self.space[7].empty(),
+            20 => self.space[19].empty(),
+            21 => self.space[20].empty(),
+            22 => self.space[21].empty(),
+            23 => self.space[7].empty() || self.space[9].empty(),
+            24 => self.space[23].empty(),
+            25 => self.space[24].empty(),
+            26 => self.space[25].empty(),
+            _ => false,
+        }
+    }
+
+    fn get_all_possible_moves(&self) -> VecDeque<Board> {
+        let mut moves = VecDeque::new();
 
         for start in 0..27 {
-            if self.space[start].occupied() {
+            if self.pod_can_move(start) {
                 for end in 0..27 {
                     if start != end {
                         if let Some(new_position) = self.move_pod(start, end) {
-                            moves.push(new_position);
+                            moves.push_back(new_position);
                         }
                     }
                 }
@@ -239,24 +268,85 @@ impl Board {
 
         moves
     }
+
+    fn finished(&self) -> bool {
+        for i in 0..27 {
+            if self.space[i] != FINISHED_BOARD.space[i] {
+                return false;
+            }
+        }
+
+        true
+    }
 }
 
+const FINISHED_BOARD: Board = Board {
+    space: [
+        Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+        A, A, A, A,
+        B, B, B, B,
+        C, C, C, C,
+        D, D, D, D,
+    ],
+    energy: 0,
+};
+
 pub fn run() {
-    println!("2021:23");
-    let board = starting_board();
+    // let finished = finished_board();
+    // let start = starting_board();
+    // println!("does it see a done board: {}", finished.finished());
+    // println!("{}", finished);
+    // println!("does it see an unfished board: {}", start.finished());
+    // println!("{}", start);
 
-    println!("{}", board);
+    // let mut test_board = Board {
+    //     space: [
+    //         Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+    //         B, D, D, B,
+    //         A, C, B, C,
+    //         A, B, A, D,
+    //         D, A, C, C,
+    //     ],
+    //     energy: 0,
+    // };
 
-    let opening_moves = board.get_all_possible_moves();
+    // test_board = test_board.move_pod(11, 0).unwrap();
+    // test_board = test_board.move_pod(23, 1).unwrap();
 
-    for board in opening_moves.iter() {
-        println!("{}", board);
+    // let boards = test_board.get_all_possible_moves();
+
+    // for board in boards.iter() {
+    //     println!("{}", board);
+    // }
+    // println!("{} moves found", boards.len());
+
+    let mut least_energy_used = u32::MAX;
+    let mut board_states: VecDeque<Board> = VecDeque::new();
+    board_states.push_back(starting_board());
+
+    while let Some(board) = board_states.pop_front() {
+        let new_boards = board.get_all_possible_moves();
+        let found = new_boards.len();
+
+        for new in new_boards {
+            if new.finished() {
+                if new.energy < least_energy_used {
+                    least_energy_used = new.energy;
+                    println!("Found a new lowest: {} energy", new.energy);
+                }
+            } else if new.energy < least_energy_used {
+                board_states.push_back(new);
+            }
+        }
+        println!("found {:#8} - queue: {}", found, board_states.len());
     }
-    println!("\nFound {} opening moves", opening_moves.len());
+
+    println!("All paths explored, least energy spent: {} energy", least_energy_used);
 }
 
 fn starting_board() -> Board {
     use Pod::*;
+
     Board {
         space: [
             Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
@@ -264,6 +354,19 @@ fn starting_board() -> Board {
             A, C, B, C,
             A, B, A, D,
             D, A, C, C,
+        ],
+        energy: 0,
+    }
+}
+
+fn finished_board() -> Board {
+    Board {
+        space: [
+            Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+            A, A, A, A,
+            B, B, B, B,
+            C, C, C, C,
+            D, D, D, D,
         ],
         energy: 0,
     }
